434eb6be674c8219b89fef0ce9e6bd5a5f3fd2ba
Akash Chavan <akashc@softype.com>
2023-04-30 21:20:04 +0530

    STMedicare9

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
+  define: {
+    'process': {env:{
+      SITE_KEY: "6Ldc5M0lAAAAAEMvzUc3JlHltquCnp0r2opVodVO"
+    }}
+  },
   server: {
     port: 3003,
   },
