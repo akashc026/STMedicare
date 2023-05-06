b5785955fe5d2d39ee25334b4bdce17b3373a645
Akash Chavan <akashc@softype.com>
2023-05-06 18:09:57 +0530

    new schedule

--- src/pages/SchedulePage.tsx
+++ src/pages/SchedulePage.tsx
@@ -8,7 +8,7 @@ import { useNavigate } from 'react-router-dom';
 export function SchedulePage(): JSX.Element {
   const navigate = useNavigate();
   const medplum = useMedplum();
-  const test_resource = medplum.readResource('Schedule','192cafd3-e5fd-4377-916b-6e3a41e9ab17').read() as Schedule;
+  const test_resource = medplum.readResource('Schedule','f83ec14c-cc12-410a-8f66-71af3e1d3273').read() as Schedule;
   const test_ques = medplum.readResource('Questionnaire','6ccaae43-47dd-4ee6-934c-7adc4a30affc').read() as Questionnaire;
   console.log(test_ques)
   return(
