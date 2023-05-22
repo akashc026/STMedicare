684921c74ff693b64e18efddcc418988d817cb39
Rohit Jha <rohitj@softype.com>
2023-05-22 13:09:50 +0530

    added states

--- src/pages/AppointmentList.tsx
+++ src/pages/AppointmentList.tsx
@@ -3,18 +3,29 @@ import { createReference, formatDateTime } from '@medplum/core';
 import { Appointment } from '@medplum/fhirtypes';
 import { Document, useMedplum } from '@medplum/react';
 import { IconCheck, IconPlaystationX } from '@tabler/icons-react';
-import React from 'react';
+import React ,{useEffect,useState} from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Loading } from '../components/Loading';
 
 export function AppointmentList(): JSX.Element {
   const medplum = useMedplum();
   const navigate = useNavigate();
   const profile = medplum.getProfile();
+  const [appointments,setAppointments]= useState<Appointment[]>()
   console.log('profile',profile)
-  const appointments = medplum.searchResources('Appointment',`practitioner=Practitioner/${profile?.id},status!=cancelled`).read();
-  console.log('appointments',appointments)
-  const handleClick = (appointment:Appointment,action:string)=>{
-    medplum.patchResource(
+  useEffect(()=>{
+      
+      const appointmentArr = medplum.searchResources('Appointment',`practitioner=Practitioner/${profile?.id},status!=cancelled`).then(
+        (data)=>{
+          setAppointments(data)
+
+        }
+      ).catch((err) => console.error(err));;
+    console.log('appointments',appointmentArr)
+  },[profile])
+ 
+  const handleClick = async(appointment:Appointment,action:string)=>{
+   await medplum.patchResource(
       'Appointment',
       appointment.id as string,
       [
@@ -34,10 +45,20 @@ export function AppointmentList(): JSX.Element {
       //   }
       // ],
       // status:action === "accepted" ? "booked" : "cancelled"
-    )
+    ).then(data=>{
+      setAppointments(prev=>{
+        let index = prev?.findIndex((ap)=>ap.id === data.id) 
+        prev![index!]= data;
+        
+      return [...prev!]
+    })}).catch(e=>{
+      console.log(e)
+      return false
+    })
   }
-
-
+  if (!appointments) {
+    return <Loading />;
+  }
   return (
     <Document>
       <h1>Appointments</h1>
